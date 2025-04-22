13de9b15a0d63664f6f75104ace6ca1fb0dd6df8
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-22 12:22:05 +0900

    Algorithm S_5_11650

--- 1_java/workspace/Algorithm/src/ex01/Main.java
+++ 1_java/workspace/Algorithm/src/ex01/Main.java
@@ -1,25 +1,43 @@
 package ex01;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine(); //3241 -> 4321
+		Scanner sc = new Scanner(System.in);
+
+		// 입력
+		int n = sc.nextInt();
+		Integer[][] arr = new Integer[n][2];
+
+		for (int i = 0; i < n; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+
+		// 정수형 배열 정렬
+		Arrays.sort(arr, new Comparator<Integer[]>() {
+
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				if (o1[0].equals(o2[0])) {
+					return o1[1] - o2[1]; //오름
+				} else {
+					return o1[0] - o2[0]; // 오름
+				}
+
+			}
+
+		});
 		
-		char[] ret = str.toCharArray(); // 문자열 어레이로 바꿈
-		
-		Arrays.sort(ret); // 오름차순 정렬
-		
-		// 내림차순 출력
-		for(int x=ret.length-1; x>=0; x--) {
-			System.out.print(ret[x]);
+		for(int i=0; i<n; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
 		}
 		
-		System.out.println("TEST!!!");
+
 	}
 }
