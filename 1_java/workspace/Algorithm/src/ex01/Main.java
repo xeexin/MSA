486d59d48f0e4eef8b570b303c7b1a0175106f1f
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-23 16:09:54 +0900

    Update Main.java

--- 1_java/workspace/Algorithm/src/ex01/Main.java
+++ 1_java/workspace/Algorithm/src/ex01/Main.java
@@ -3,37 +3,32 @@ package ex01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+
 		int n = Integer.parseInt(br.readLine());
-		
-		
+
 		TreeMap<String, String> arr = new TreeMap<String, String>();
-		
-		for(int x=0; x<n; x++) {
+
+		for (int x = 0; x < n; x++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String name = st.nextToken();
 			String status = st.nextToken();
-			
+
 			arr.put(name, status); // key 값 중복 시 마지막 값으로 update
 		}
-		
+
 		// 역순 출력 (Z ~ A)
-				for (String name : arr.descendingKeySet()) {
-					if (arr.get(name).equals("enter")) {
-						System.out.println(name);
-					}
-				}
+		for (String name : arr.descendingKeySet()) {
+			if (arr.get(name).equals("enter")) {
+				System.out.println(name);
+			}
+		}
 
 	}
 }
