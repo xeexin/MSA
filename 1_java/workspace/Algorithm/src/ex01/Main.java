d1f9e5f7a413de410bd7992538e23551e4faf21a
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-25 17:19:58 +0900

    Algorithm

--- 1_java/workspace/Algorithm/src/ex01/Main.java
+++ 1_java/workspace/Algorithm/src/ex01/Main.java
@@ -3,47 +3,24 @@ package ex01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()); // 3 5 이렇게 들어오니까 st필요
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
 
-		HashSet<Integer> setA = new HashSet<>();
-		HashSet<Integer> setB = new HashSet<>();
-
-		st = new StringTokenizer(br.readLine()); // 1 2 4
 		for (int x = 0; x < n; x++) {
-			setA.add(Integer.parseInt(st.nextToken()));
-		}
+			String str = br.readLine();
+			String[] arr = str.split(" ");
 
-		st = new StringTokenizer(br.readLine()); // 2 3 4 5 6
-
-		for (int x = 0; x < m; x++) {
-			setB.add(Integer.parseInt(st.nextToken()));
-		}
-
-		int cnt = 0;
-
-		for (int num : setA) {
-			if (!setB.contains(num)) {
-				cnt += 1;
+			System.out.println("Case #" + (x + 1) + ": ");
+			
+			for (int y = arr.length - 1; y >= 0; y--) {
+				System.out.print(arr[y] + " ");
 			}
+			System.out.println();
 		}
-
-		for (int num : setB) {
-			if (!setA.contains(num)) {
-				cnt += 1;
-			}
-		}
-
-		System.out.println(cnt);
 	}
 }
---
+++ 1_java/workspace/day27_jdbc_quiz/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module day27_jdbc_quiz {
+}
