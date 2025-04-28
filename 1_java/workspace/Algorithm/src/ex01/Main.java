3529480dffa5f4e44bad6d29ae5eff44ef6c9aa3
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-28 17:57:09 +0900

    Algorithm_B1_1934

--- 1_java/workspace/Algorithm/src/ex01/Main.java
+++ 1_java/workspace/Algorithm/src/ex01/Main.java
@@ -3,24 +3,41 @@ package ex01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		/**
+		 * 유클리드 호제법이란 무엇인가? 큰 수를 작은 수로 나눈다.
+		 * 
+		 * 나누는 수를 나머지로 계속 나눈다.
+		 * 
+		 * 나머지가 0이 나오면 나누는 수가 최대공약수이다.
+		 */
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
+		int t = Integer.parseInt(br.readLine());
 
-		for (int x = 0; x < n; x++) {
-			String str = br.readLine();
-			String[] arr = str.split(" ");
+		for (int x = 0; x < t; x++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-			System.out.print("Case #" + (x + 1) + ": ");
-			
-			for (int y = arr.length - 1; y >= 0; y--) {
-				System.out.print(arr[y] + " ");
-			}
-			System.out.println();
+			int k = solution(a, b);
+			int ret = a*b / k;
+			System.out.println(ret);
 		}
+
+	}
+
+	static int solution(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
 	}
 }
