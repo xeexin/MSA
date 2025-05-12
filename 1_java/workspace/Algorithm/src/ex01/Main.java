0ce71eccbd953d114b5d6387655a51817b27e1be
HEEJIN LIM <xeexin0331@gmail.com>
2025-05-12 15:08:08 +0900

    프로그래머스:올바른 괄호

--- 1_java/workspace/Algorithm/src/ex01/Main.java
+++ 1_java/workspace/Algorithm/src/ex01/Main.java
@@ -1,31 +1,50 @@
 package ex01;
 
+/**
+ * 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+"()()" 또는 "(())()" 는 올바른 괄호입니다.
+")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 
+문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+문자열 s의 길이 : 100,000 이하의 자연수
+문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Solution {
+	boolean solution(String s) {
+		boolean answer = true;
+
+		Stack<Character> stack = new Stack<>();
+		for (int x = 0; x < s.length(); x++) {
+			char a = s.charAt(x);
+
+			if (a == '(') {
+				stack.push(a);
+			}
+
+			else {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				stack.pop();
+			}
+		}
+
+		return stack.isEmpty();
+	}
+}
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
-		String[] str = new String[n];
-
-		for (int x = 0; x < n; x++) {
-			str[x] = br.readLine();
-		}
-
-		for (int x = 0; x < n; x++) {
-			char first = str[x].charAt(0);
-			if (first >= 'a' && first <= 'z') {
-				first = (char) (first - ('a' - 'A'));
-				str[x] = first + str[x].substring(1);
-			}
-		}
-
-		for (int x = 0; x < n; x++) {
-			System.out.println(str[x]);
-		}
 
 	}
 }
