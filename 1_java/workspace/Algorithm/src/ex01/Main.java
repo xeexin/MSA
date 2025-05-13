d8229fad3b76d1ae0a2891f2de4b2f12f04a60b0
HEEJIN LIM <xeexin0331@gmail.com>
2025-05-13 14:16:49 +0900

    프로그래머스 : 최솟값 구하기

--- 1_java/workspace/Algorithm/src/ex01/Main.java
+++ 1_java/workspace/Algorithm/src/ex01/Main.java
@@ -1,44 +1,29 @@
 package ex01;
 
-/**
- * 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
-
-"()()" 또는 "(())()" 는 올바른 괄호입니다.
-")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
-'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 
-문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
-
-제한사항
-문자열 s의 길이 : 100,000 이하의 자연수
-문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
-
- */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.Arrays;
 
 class Solution {
-	boolean solution(String s) {
-		boolean answer = true;
+	public int solution(int[] A, int[] B) {
+		int answer = 0;
 
-		Stack<Character> stack = new Stack<>();
-		for (int x = 0; x < s.length(); x++) {
-			char a = s.charAt(x);
+		Arrays.sort(A); // 오름차순
+		Arrays.sort(B);
 
-			if (a == '(') {
-				stack.push(a);
-			}
-
-			else {
-				if (stack.isEmpty()) {
-					return false;
-				}
-				stack.pop();
-			}
+		// 뒤집기
+		for (int x = 0; x < B.length / 2; x++) {
+			int tmp = B[x];
+			B[x] = B[B.length - 1 - x];
+			B[B.length - 1 - x] = tmp;
 		}
 
-		return stack.isEmpty();
+		for (int x = 0; x < A.length; x++) {
+			answer += (A[x] * B[x]);
+		}
+
+		return answer;
 	}
 }
 
